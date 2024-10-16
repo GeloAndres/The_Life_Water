f930c31a636fa842a91aa75b880b52090c580ca3
Luis Toro <luis.toro.f@usach.cl>
2024-10-16 00:45:45 -0300

    CrossOrigin 2

--- Backend/agua/src/main/java/proyecto/agua/controllers/UbicacionController.java
+++ Backend/agua/src/main/java/proyecto/agua/controllers/UbicacionController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +13,7 @@ import proyecto.agua.services.UbicacionService;
 
 @Controller
 @RequestMapping("/ubicacion")
+@CrossOrigin(origins = "*")
 public class UbicacionController {
     
     @Autowired
