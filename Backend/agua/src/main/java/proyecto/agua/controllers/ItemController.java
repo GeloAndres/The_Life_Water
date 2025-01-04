911ee4aa027c400dcb64d09ec68d7ea6f46b01cc
Luis Javier Toro Flores <luis.toro.f@usach.cl>
2025-01-03 23:04:47 -0300

    RequestBody de item y pedido

--- Backend/agua/src/main/java/proyecto/agua/controllers/ItemController.java
+++ Backend/agua/src/main/java/proyecto/agua/controllers/ItemController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import proyecto.agua.entities.ItemEntity;
@@ -30,7 +31,7 @@ public class ItemController {
 
     // Post de un item
     @PostMapping
-    public ResponseEntity<ItemEntity> saveItem(ItemEntity item){
+    public ResponseEntity<ItemEntity> saveItem(@RequestBody ItemEntity item){
         ItemEntity itemSaved = itemService.saveItem(item);
         if(itemSaved == null){
             return ResponseEntity.badRequest().build();
--- Backend/agua/src/main/java/proyecto/agua/controllers/PedidoController.java
+++ Backend/agua/src/main/java/proyecto/agua/controllers/PedidoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import proyecto.agua.entities.PedidoEntity;
@@ -30,7 +31,7 @@ public class PedidoController {
 
     // Post de un pedido
     @PostMapping
-    public ResponseEntity<PedidoEntity> savePedido(PedidoEntity pedido){
+    public ResponseEntity<PedidoEntity> savePedido(@RequestBody PedidoEntity pedido){
         PedidoEntity pedidoSaved = pedidoService.savePedido(pedido);
         if(pedidoSaved == null){
             return ResponseEntity.badRequest().build();
