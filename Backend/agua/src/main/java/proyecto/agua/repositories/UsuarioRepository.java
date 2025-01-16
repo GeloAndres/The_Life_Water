8577250e2fe48e265ccbb7d75efc0dfe528ca775
Luis Javier Toro Flores <luis.toro.f@usach.cl>
2025-01-16 01:46:08 -0300

    Delete usuario

--- Backend/agua/src/main/java/proyecto/agua/repositories/UsuarioRepository.java
+++ Backend/agua/src/main/java/proyecto/agua/repositories/UsuarioRepository.java
@@ -1,6 +1,9 @@
 package proyecto.agua.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import proyecto.agua.entities.UsuarioEntity;
@@ -8,4 +11,6 @@ import proyecto.agua.entities.UsuarioEntity;
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
     
+    @Query(value = "SELECT * FROM usuario WHERE usuario.borrado = 0", nativeQuery = true)
+    List<UsuarioEntity> getUsuarios();
 }
