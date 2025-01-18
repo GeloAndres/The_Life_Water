04583eb2c232202582791cb8c667891c1766fe75
Gelo Figueroa <gelofiguero@gmail.com>
2025-01-18 12:48:47 -0300

    implementar NotifierProviderState de ordenes de compra

--- frontend/the_life_water/lib/presentation/Provider/pedido_provider.dart
+++ frontend/the_life_water/lib/presentation/Provider/pedido_provider.dart
@@ -1,1 +1,31 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:the_life_water/domain/entities/order.dart';
 
+final pedidoStateProvider =
+    StateNotifierProvider<ListNotifier, List<Order>>(((ref) => ListNotifier()));
+
+class ListNotifier extends StateNotifier<List<Order>> {
+  ListNotifier() : super([]);
+
+  void agregarPedido(Order orden) {
+    if (state.isEmpty) {
+      orden.idCompra == 1;
+    } else {
+      orden.idCompra == state.length++;
+    }
+
+    state = [...state, orden];
+  }
+
+  void borrarOrden(int idCompra) {
+    state.removeWhere((orden) => orden.idCompra == idCompra);
+  }
+
+  void modificarOrden(Order ordenModificado) {
+    state.map((orden) {
+      if (orden.idCompra == ordenModificado.idCompra) {
+        return ordenModificado;
+      }
+    });
+  }
+}
